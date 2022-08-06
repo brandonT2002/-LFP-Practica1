a81d3f0462140069d9cff75ad6c5d73b35652242
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2022-08-05 18:25:10 -0600

    agregando funcionalidad al boton cargar archivo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import tkinter
-from tkinter import font
-import tkinter.messagebox
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
 import customtkinter
 
 customtkinter.set_appearance_mode("System")
@@ -24,45 +24,64 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.frame_left = customtkinter.CTkFrame(master=self,width=180,corner_radius=0)
-        self.frame_left.grid(row=0,column=0,sticky="nswe")
+        self.frameLeft = customtkinter.CTkFrame(master=self,width=180,corner_radius=0)
+        self.frameLeft.grid(row=0,column=0,sticky="nswe")
 
-        self.frame_right = customtkinter.CTkFrame(master=self)
-        self.frame_right.grid(row=0,column=1,sticky="nswe",padx=20,pady=20)
+        self.frameRight = customtkinter.CTkFrame(master=self)
+        self.frameRight.grid(row=0,column=1,sticky="nswe",padx=20,pady=20)
 
         # ============ frame_left ============
         # configure grid layout (1x11)
-        self.frame_left.grid_rowconfigure(0, minsize=10)
-        self.frame_left.grid_rowconfigure(5, weight=1)
-        self.frame_left.grid_rowconfigure(8, minsize=20)
-        self.frame_left.grid_rowconfigure(11, minsize=10)
+        self.frameLeft.grid_rowconfigure(0, minsize=10)
+        self.frameLeft.grid_rowconfigure(5, weight=1)
+        self.frameLeft.grid_rowconfigure(8, minsize=20)
+        self.frameLeft.grid_rowconfigure(11, minsize=10)
 
-        self.options = customtkinter.CTkLabel(master=self.frame_left,text="Opciones",text_font=("Roboto Medium",16))
-        self.options.grid(row=1,column=0,pady=10, padx=10)
+        self.options = customtkinter.CTkLabel(master=self.frameLeft,text="Opciones",text_font=("Roboto Medium",16))
+        self.options.grid(row=1,column=0,pady=10,padx=10)
 
-        self.fileUpload = customtkinter.CTkButton(master=self.frame_left,text="Cargar Archivos",command=self.button_event)
+        self.fileUpload = customtkinter.CTkButton(master=self.frameLeft,text="Cargar Archivos",command=self.selectFile)
         self.fileUpload.grid(row=2,column=0,pady=10,padx=20)
 
-        self.manageCourses = customtkinter.CTkButton(master=self.frame_left,text="Gestionar Cursos",command=self.button_event)
+        self.manageCourses = customtkinter.CTkButton(master=self.frameLeft,text="Gestionar Cursos",command=self.button_event)
         self.manageCourses.grid(row=3,column=0,pady=10,padx=20)
 
-        self.credits = customtkinter.CTkButton(master=self.frame_left,text="Conteo de Creditos",command=self.button_event)
+        self.credits = customtkinter.CTkButton(master=self.frameLeft,text="Conteo de Creditos",command=self.button_event)
         self.credits.grid(row=4,column=0,pady=10,padx=20)
 
-        self.credits = customtkinter.CTkButton(master=self.frame_left,text="Salir",fg_color="#D35B58",hover_color="#C77C78",command=self.quit)
+        self.credits = customtkinter.CTkButton(master=self.frameLeft,text="Salir",fg_color="#D35B58",hover_color="#C77C78",command=self.quit)
         self.credits.grid(row=9,column=0,pady=10,padx=20)
 
         # ============ frame_right ============
-
         # configure grid layout (3x7)
-        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
-        self.frame_right.rowconfigure(7, weight=10)
-        self.frame_right.columnconfigure((0, 1), weight=1)
-        self.frame_right.columnconfigure(2, weight=0)
+        self.frameRight.rowconfigure((0, 1, 2, 3), weight=1)
+        self.frameRight.rowconfigure(7, weight=10)
+        self.frameRight.columnconfigure((0, 1), weight=1)
+        self.frameRight.columnconfigure(2, weight=0)
 
-        self.data = customtkinter.CTkLabel(master=self.frame_right,text="Lenguajes Formales y de Programación\n" +"Brandon Andy J. Tejaxún Pichiyá\n" +"202112030",height=100,corner_radius=6,fg_color=("white", "gray38"),justify=tkinter.LEFT)
+        self.data = customtkinter.CTkLabel(master=self.frameRight,text="Lenguajes Formales y de Programación\nBrandon Andy J. Tejaxún Pichiyá\n202112030",height=70,corner_radius=6,fg_color=("white", "gray38"),justify=tkinter.CENTER)
         self.data.grid(column=0,row=0,columnspan=2,rowspan=4,sticky="nwe",padx=20,pady=20)
         self.data.config(font=('Roboto Medium',13))
+
+        self.route = customtkinter.CTkEntry(master=self.frameRight,width=120,placeholder_text="Ruta del archivo")
+        self.route.grid(row=2,column=0,columnspan=2,pady=20,padx=20,sticky="we")
+
+    def selectFile(self):
+        self.route.configure(state=tkinter.NORMAL)
+        self.route.delete(0, 'end')
+        filetypes = (
+            ('text files', '*.csv'),
+            ('All files', '*.*')
+        )
+        fileRoute = fd.askopenfilename(
+            title='Open a file',
+            initialdir='',
+            filetypes=filetypes)
+        fichero = open(fileRoute,'r')
+        text = fichero.read()
+        print(text)
+        self.route.insert(0,str(fileRoute))
+        self.route.configure(state=tkinter.DISABLED)
 
     def button_event(self):
         print("Button pressed")
