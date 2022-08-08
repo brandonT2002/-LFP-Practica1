cf5a55a0a0e6d2b5452212723dc95e3f398451fa
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2022-08-07 18:41:21 -0600

    restructuring interface

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import tkinter
 from tkinter import filedialog as fd
 import customtkinter
+from readData import Controller
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 
 class App(customtkinter.CTk):
-
     WIDTH = 1250
-    HEIGHT = 520
+    HEIGHT = 600
 
     def __init__(self):
         super().__init__()
 
         self.title("Practica1 - LFP")
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.protocol("WM_DELETE_WINDOW", self.onClosing)
         self.minsize(App.WIDTH,App.HEIGHT)
 
         # ============ create two frames ============
@@ -25,13 +25,10 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
 
         self.frameLeft = customtkinter.CTkFrame(master=self,width=180,corner_radius=0)
-        self.frameLeft.grid(row=0,column=0,sticky="nswe")
+        self.frameLeft.grid(row=0,column=0,sticky="nswe")  
 
         self.frameRight1 = customtkinter.CTkFrame(master=self)
         self.frameRight1.grid(row=0,column=1,sticky="nswe",padx=20,pady=20)
-
-        #self.frameRight2 = customtkinter.CTkFrame(master=self)
-        #self.frameRight2.grid(row=0,column=1,sticky="nswe",padx=20,pady=20)
 
         self.panelOptions()
         self.panelFileUpload()
@@ -50,10 +47,10 @@ class App(customtkinter.CTk):
         self.fileUpload = customtkinter.CTkButton(master=self.frameLeft,text="Cargar Archivos",command=self.selectFile)
         self.fileUpload.grid(row=2,column=0,pady=10,padx=20)
 
-        self.manageCourses = customtkinter.CTkButton(master=self.frameLeft,text="Gestionar Cursos",command=self.button_event)
+        self.manageCourses = customtkinter.CTkButton(master=self.frameLeft,text="Gestionar Cursos",command=self.buttonEvent)
         self.manageCourses.grid(row=3,column=0,pady=10,padx=20)
 
-        self.credits = customtkinter.CTkButton(master=self.frameLeft,text="Conteo de Creditos",command=self.button_event)
+        self.credits = customtkinter.CTkButton(master=self.frameLeft,text="Conteo de Creditos",command=self.buttonEvent)
         self.credits.grid(row=4,column=0,pady=10,padx=20)
 
         self.credits = customtkinter.CTkButton(master=self.frameLeft,text="Salir",fg_color="#D35B58",hover_color="#C77C78",command=self.quit)
@@ -62,25 +59,41 @@ class App(customtkinter.CTk):
     def panelFileUpload(self):
         # ============ frame_right ============
         # configure grid layout (3x7)
-        self.frameRight1.rowconfigure((0, 1, 2, 3), weight=1)
-        self.frameRight1.rowconfigure(7, weight=10)
-        self.frameRight1.columnconfigure((0, 1), weight=1)
-        self.frameRight1.columnconfigure(2, weight=0)
+        self.frameRight1.rowconfigure((0, 1, 2, 3, 4), weight=1)
+        self.frameRight1.rowconfigure(5, weight=10)
+        self.frameRight1.columnconfigure((0, 1, 2, 3, 4), weight=1)
+        self.frameRight1.columnconfigure(5, weight=0)
         """
         self.data = customtkinter.CTkLabel(master=self.frameRight1,text="Lenguajes Formales y de Programación\nBrandon Andy J. Tejaxún Pichiyá\n202112030",height=70,corner_radius=6,fg_color=("white", "gray38"),justify=tkinter.CENTER)
         self.data.grid(column=0,row=0,columnspan=2,rowspan=4,sticky="nwe",padx=20,pady=20)
         self.data.config(font=('Roboto Medium',13))
         """
+        self.showCourses = customtkinter.CTkButton(master=self.frameRight1,text="Mostrar Cursos",fg_color="gray40",hover_color="gray25",command=self.buttonEvent)
+        self.showCourses.grid(row=0,column=0,pady=10,padx=5)
+
+        self.addCourse = customtkinter.CTkButton(master=self.frameRight1,text="Agregar Curso",fg_color="gray40",hover_color="gray25",command=self.buttonEvent)
+        self.addCourse.grid(row=0,column=1,pady=10,padx=5)
+
+        self.editCourse = customtkinter.CTkButton(master=self.frameRight1,text="Editar Curso",fg_color="gray40",hover_color="gray25",command=self.buttonEvent)
+        self.editCourse.grid(row=0,column=2,pady=10,padx=5)
+
+        self.deleteCourse = customtkinter.CTkButton(master=self.frameRight1,text="Eliminar Curso",fg_color="gray40",hover_color="gray25",command=self.buttonEvent)
+        self.deleteCourse.grid(row=0,column=3,pady=10,padx=5)
+
+        self.toReturn = customtkinter.CTkButton(master=self.frameRight1,text="Regresar",fg_color="gray40",hover_color="gray25",command=self.buttonEvent)
+        self.toReturn.grid(row=0,column=4,pady=10,padx=5)
+
         self.route = customtkinter.CTkEntry(master=self.frameRight1,width=120,placeholder_text="Ruta del archivo")
-        self.route.grid(row=0,column=0,columnspan=2,pady=20,padx=20,sticky="nwe")
+        self.route.grid(row=1,column=0,columnspan=5,pady=20,padx=20,sticky="nwe")
+
         self.tableCourses()
+
 
     def tableCourses(self):
         self.table = customtkinter.CTkFrame(master=self.frameRight1)
-        self.table.grid(row=1,column=0,columnspan=2,padx=20,sticky="nsew")
+        self.table.grid(row=2,column=0,columnspan=5,padx=20,sticky="nsew")
         self.table.rowconfigure(0, weight=1)
-        self.table.columnconfigure(0, weight=1)
-        
+        self.table.columnconfigure(0, weight=1)        
 
         self.text = customtkinter.CTkLabel(master=self.table,text = "Código").grid(row=0,column=0)
         self.text = customtkinter.CTkLabel(master=self.table,text = "Nombre").grid(row=0,column=1)
@@ -89,35 +102,49 @@ class App(customtkinter.CTk):
         self.text = customtkinter.CTkLabel(master=self.table,text = "Semestre").grid(row=0,column=4)
         self.text = customtkinter.CTkLabel(master=self.table,text = "Creditos").grid(row=0,column=5)
         self.text = customtkinter.CTkLabel(master=self.table,text = "Estado").grid(row=0,column=6)
-        self.results=[['017','Social Humanística 1','','1','1','4','0'],['147','Física Básica','101','1','2','5','0'],['150','Física 1','103,147','1','3','6','1']]
+        self.results=[
+            ['017','Social Humanística 1','','1','1','4','0'],
+            ['017','Social Humanística 1','','1','1','4','0'],
+            ['017','Social Humanística 1','','1','1','4','0'],
+            ['147','Física Básica','101','1','2','5','0'],
+            ['147','Física Básica','101','1','2','5','0'],
+            ['147','Física Básica','101','1','2','5','0'],
+            ['147','Física Básica','101','1','2','5','0'],
+            ['147','Física Básica','101','1','2','5','0'],
+            ['147','Física Básica','101','1','2','5','0'],
+            ['147','Física Básica','101','1','2','5','0'],
+            ['150','Física 1','103,147','1','3','6','1'],
+            ['150','Física 1','103,147','1','3','6','1'],
+            ['150','Física 1','103,147','1','3','6','1'],
+            ['150','Física 1','103,147','1','3','6','1'],
+            ['150','Física 1','103,147','1','3','6','1']]
         for i in range(len(self.results)):
-            for j in range(7): # here we use dynamic variable so that you can edit and access the table elements if needed
+            for j in range(7):
                 exec(f"self.e{i}_{j} = customtkinter.CTkEntry(self.table,width=150)")
                 exec(f"self.e{i}_{j}.grid(row={i}+1,column={j},columnspan=1,sticky='we')")
                 exec("self.e"+str(i)+"_"+str(j)+".insert('end',self.results["+str(i)+"]["+str(j)+"])")
+                exec(f"self.e{i}_{j}.configure(state=tkinter.DISABLED)")
 
     def selectFile(self):
         self.route.configure(state=tkinter.NORMAL)
-        self.route.delete(0, 'end')
         filetypes = (
-            ('text files', '*.csv'),
+            ('text files', '*.lfp'),
             ('All files', '*.*')
         )
         fileRoute = fd.askopenfilename(
             title='Open a file',
             initialdir='',
             filetypes=filetypes)
-        fichero = open(fileRoute,'r')
-        text = fichero.read()
-        print(text)
+        self.route.delete(0, 'end')
+        Controller().readFile(fileRoute)
         self.route.insert(0,str(fileRoute))
         self.route.configure(state=tkinter.DISABLED)
 
-    def button_event(self):
+    def buttonEvent(self):
         pass
-        #self.frameRight2.grid_forget()
+        #self.frameRight1.grid_forget()
 
-    def on_closing(self, event=0):
+    def onClosing(self, event=0):
         self.destroy()
 
 
--- readData.py
+++ readData.py
@@ -4,8 +4,8 @@ class Controller:
     def __init__(self):
         self.courses = []
 
-    def readFile(self):
-        cursos = open('cursos.lfp',encoding='utf-8').read().split('\n')
+    def readFile(self,route):
+        cursos = open(route,encoding='utf-8').read().split('\n')
         for course in cursos:
             course = course.split(',')
             self.courses.append(Course(int(course[0]),course[1],course[2].split(';'),int(course[3]),int(course[4]),int(course[5]),course[6]))
